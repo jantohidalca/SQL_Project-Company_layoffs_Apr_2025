6eb26aae1aa51cfe7bdd4c3b6df41e842b5e1af6
guefusag2 <jantohidalca@gmail.com>
2025-07-27 16:32:16 +0200

    Adding all necessary files

---
+++ CREATING_DATABASE/2.create_table.sql
@@ -0,0 +1,15 @@
+CREATE TABLE public.layoffs (
+    company TEXT,
+    location TEXT,
+    total_laid_off NUMERIC,
+    "date" TEXT,
+    percentage_laid_off TEXT,
+    industry TEXT,
+    source TEXT,
+    stage TEXT,
+    funds_raised TEXT,
+    country TEXT,
+    date_added TEXT
+)
+
+-- DROP TABLE IF EXISTS layoffs;
---
+++ CREATING_DATABASE/3.transform_table.sql
@@ -0,0 +1,3 @@
+-- \copy layoffs FROM 'C:\Users\Lenovo\Documents\Data Analyst\SQL\SQL_Project_Layoffs\layoffs.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+SELECT * FROM layoffs;
